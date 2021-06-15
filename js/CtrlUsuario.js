0138b1658a5ac39c997cdbf739a98b6ee2b15d62
IngDevLuis <84766065+IngDevLuis@users.noreply.github.com>
2021-06-14 20:55:59 -0500

    Update CtrlUsuario.js

--- js/CtrlUsuario.js
+++ js/CtrlUsuario.js
@@ -1,1 +1,190 @@
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  eliminaStorage,
+  urlStorage
+} from "../lib/storage.js";
+import {
+  muestraError
+} from "../lib/util.js";
+import {
+  muestraUsuarios
+} from "./navegacion.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+import {
+  checksRoles,
+  guardaUsuario,
+  selectEquipos
+} from "./usuarios.js";
 
+const params =
+  new URL(location.href).
+    searchParams;
+const id = params.get("id");
+const daoEquipos = getFirestore().
+  collection("Equipos");
+/** @type {HTMLFormElement} */
+const forma = document["forma"];
+const img = document.
+  querySelector("img");
+/** @type {HTMLUListElement} */
+const listaRoles = document.
+  querySelector("#listaRoles");
+getAuth().onAuthStateChanged(
+  protege, muestraError);
+
+/** @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  if (tieneRol(usuario,
+    ["Administrador"])) {
+    busca();
+  }
+}
+
+async function busca() {
+  try {
+    const doc = await daoEquipos.
+      doc(id).
+      get();
+    if (doc.exists) {
+      const data = doc.data();
+      forma.cue.value = id || "";
+      img.src =
+        await urlStorage(id);
+      selectEquipos(
+        forma.equiposId,
+        data.equiposId)
+      checksRoles(
+        listaRoles, data.rolIds);
+      forma.addEventListener(
+        "submit", guarda);
+      forma.eliminar.
+        addEventListener(
+          "click", elimina);
+    }
+  } catch (e) {
+    muestraError(e);
+    muestraUsuarios();
+  }
+}
+
+/** @param {Event} evt */
+async function guarda(evt) {
+  await guardaUsuario(evt,
+    new FormData(forma), id);
+}
+
+async function elimina() {
+  try {
+    if (confirm("Confirmar la " +
+      "eliminación")) {
+      await daoUsuario.
+        doc(id).delete();
+      await eliminaStorage(id);
+      muestraUsuarios();
+    }
+  } catch (e) {
+    muestraError(e);
+  }
+}
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  eliminaStorage,
+  urlStorage
+} from "../lib/storage.js";
+import {
+  muestraError
+} from "../lib/util.js";
+import {
+  muestraUsuarios
+} from "./navegacion.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+import {
+  checksRoles,
+  guardaUsuario,
+  selectEquipos
+} from "./usuarios.js";
+
+const params =
+  new URL(location.href).
+    searchParams;
+const id = params.get("id");
+const daoEquipos = getFirestore().
+  collection("Equipos");
+/** @type {HTMLFormElement} */
+const forma = document["forma"];
+const img = document.
+  querySelector("img");
+/** @type {HTMLUListElement} */
+const listaRoles = document.
+  querySelector("#listaRoles");
+getAuth().onAuthStateChanged(
+  protege, muestraError);
+
+/** @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  if (tieneRol(usuario,
+    ["Administrador"])) {
+    busca();
+  }
+}
+
+async function busca() {
+  try {
+    const doc = await daoEquipos.
+      doc(id).
+      get();
+    if (doc.exists) {
+      const data = doc.data();
+      forma.cue.value = id || "";
+      img.src =
+        await urlStorage(id);
+      selectEquipos(
+        forma.equiposId,
+        data.equiposId)
+      checksRoles(
+        listaRoles, data.rolIds);
+      forma.addEventListener(
+        "submit", guarda);
+      forma.eliminar.
+        addEventListener(
+          "click", elimina);
+    }
+  } catch (e) {
+    muestraError(e);
+    muestraUsuarios();
+  }
+}
+
+/** @param {Event} evt */
+async function guarda(evt) {
+  await guardaUsuario(evt,
+    new FormData(forma), id);
+}
+
+async function elimina() {
+  try {
+    if (confirm("Confirmar la " +
+      "eliminación")) {
+      await daoUsuario.
+        doc(id).delete();
+      await eliminaStorage(id);
+      muestraUsuarios();
+    }
+  } catch (e) {
+    muestraError(e);
+  }
+}
